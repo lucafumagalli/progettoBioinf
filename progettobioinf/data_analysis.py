7441ff36b78dd0cf696f888a76180b957535138f
Luca <fumagalli.luca96@gmail.com>
2020-06-09 10:54:01 +0200

    added distribution plot

--- progettobioinf/data_analysis.py
+++ progettobioinf/data_analysis.py
@@ -8,6 +8,7 @@ from minepy import MINE
 import numpy as np
 import seaborn as sns
 from scipy.stats import entropy
+from sklearn.metrics.pairwise import euclidean_distances
 
 p_value_threshold = 0.01
 correlation_threshold = 0.05
@@ -154,3 +155,45 @@ def seaborn_plot_least_correlated(epigenomes, labels, scores, cell_line):
             labels[region],
         ], axis=1), hue=labels[region].columns[0])
         plt.savefig(cell_line + '/seaborn_plot_' + region +'_least.png')
+
+def get_top_most_different(epigenomes, labels, cell_line):
+    top_number = 5
+    for region, x in epigenomes.items():
+        dist = euclidean_distances(x.T)
+        most_distance_columns_indices = np.argsort(-np.mean(dist, axis=1).flatten())[:top_number]
+        columns = x.columns[most_distance_columns_indices]
+        fig, axes = plt.subplots(nrows=1, ncols=5, figsize=(25, 5))
+        print(f"Top {top_number} different features from {region}.")
+        for column, axis in zip(columns, axes.flatten()):
+            head, tail = x[column].quantile([0.05, 0.95]).values.ravel()
+            
+            mask = ((x[column] < tail) & (x[column] > head)).values
+            
+            cleared_x = x[column][mask]
+            cleared_y = labels[region].values.ravel()[mask]
+            
+            cleared_x[cleared_y==0].hist(ax=axis, bins=20)
+            cleared_x[cleared_y==1].hist(ax=axis, bins=20)
+
+            axis.set_title(column)
+        fig.tight_layout()
+        plt.savefig(cell_line + '/feature_distribution.png')
+
+def get_top_most_different_tuples(epigenomes, labels, cell_line):
+    top_number = 5
+    for region, x in epigenomes.items():
+        dist = euclidean_distances(x.T)
+        dist = np.triu(dist)
+        tuples = list(zip(*np.unravel_index(np.argsort(-dist.ravel()), dist.shape)))[:top_number]
+        fig, axes = plt.subplots(nrows=1, ncols=5, figsize=(25, 5))
+        print(f"Top {top_number} different tuples of features from {region}.")
+        for (i, j), axis in zip(tuples, axes.flatten()):
+            column_i = x.columns[i]
+            column_j = x.columns[j]
+            for column in (column_i, column_j):
+                head, tail = x[column].quantile([0.05, 0.95]).values.ravel()
+                mask = ((x[column] < tail) & (x[column] > head)).values
+                x[column][mask].hist(ax=axis, bins=20, alpha=0.5)
+            axis.set_title(f"{column_i} and {column_j}")
+        fig.tight_layout()
+        plt.savefig(cell_line + '/top_most_different.png')
